1cf8d621b50575a164b6d7c9bd16d52cc16ae0da
AmarKaur08 <101163444+AmarKaur08@users.noreply.github.com>
2022-12-14 23:57:00 +0530

    3 files modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import "./style.css";
+// import "./style.css";
+import Navigation from "./Components/Navigation.js";
+import Logo from "./Components/Logo.js"
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <Navigation />
+      <Logo/>
+      {/* <ImageLinkForm/>
+      <FaceDetector/> */}
     </div>
   );
 }
---
+++ src/Components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Styling/Logo.css';
+import tilt from "react-parallax-tilt";
+
+const Logo=()=>{
+  return(
+    <h1>logo</h1>
+  );
+}
+export default Logo
