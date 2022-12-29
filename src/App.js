be8ca0b770bd8be57876c2c6e662bad286f4e930
AmarKaur08 <101163444+AmarKaur08@users.noreply.github.com>
2022-12-29 19:49:48 +0530

    4 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Navigation from "./Components/Navigation.js";
 import Logo from "./Components/Logo.js";
 import ImageLinkForm from './Components/ImageLinkForm'
 import Rank from "./Components/Rank.js";
- import ParticlesBg from 'particles-bg';
+import ParticlesBg from 'particles-bg';
+import FaceDetector from './Components/FaceDetector'
 
 class App extends Component {
   constructor(){
     super();
     this.state={
       input:'',
+      imageUrl:''
     }
   }
   onInputChange=(event)=>
   {
     console.log(event.target.value);
+    this.setState({input:event.target.value})
   }
   onButtonSubmit=()=>{
   console.log('click');
+  this.setState({imageUrl:this.state.input})
   }
   render(){
   return (
@@ -28,7 +32,7 @@ class App extends Component {
       <Logo/>  
       <Rank/>    
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>      
-      {/* <FaceDetector/> */}
+      <FaceDetector imageUrl={this.state.imageUrl}/>
     </div>
   );
   }
--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -6,7 +6,7 @@ const Logo=()=>{
   return(
     <Tilt>
       <div className="logodecor">
-        <img src="https://previews.123rf.com/images/lumut/lumut1702/lumut170201633/72830468-brain-logo-template.jpg" alt="logo"/>        
+        <img id="imghere" src="https://previews.123rf.com/images/lumut/lumut1702/lumut170201633/72830468-brain-logo-template.jpg" alt="logo"/>        
       </div>
     </Tilt>
   );
